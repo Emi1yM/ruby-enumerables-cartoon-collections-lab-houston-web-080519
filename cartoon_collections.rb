e6abf9a3f7dfdd4dfa0545185a44eaf936fcf9ef
Emily M <mle.jo.11@gmail.com>
2019-08-02 15:37:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |item, index|
+  hash[item] = index
+}
+  puts array
 end
 
 def summon_captain_planet# code an argument here
